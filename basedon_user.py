388437e22b25bbdb28377bd14d29cd617bf58294
A.Safarji <a.m.safarji@gmail.com>
2021-12-08 13:42:14 +0300

    Update basedon_user.py
    NLP

--- basedon_user.py
+++ basedon_user.py
@@ -4,52 +4,8 @@ import pandas as pd
 import tensorflow as tf
 import logging
 import pandas as pd
-
-	
-	
-
-	
-	
-#pio.renderers.default = 'chrome'
-st.set_option('deprecation.showPyplotGlobalUse', False)
-#st.set_page_config(layout="wide")
-
-#st.title('Recommended for you!')
-st.markdown(' <p align="center" class="big-font">  <b>Authorship Attribution <u> 🌟 T5 🇸🇦</b>   </p>', unsafe_allow_html=True)	
-
-
-st.markdown("""
-<style>
-.big-font {
-    font-size:50px !important;
-}
-</style>
-""", unsafe_allow_html=True)
-
-
-
-st.markdown("""
-إسناد التأليف العربي هو مهمة البحث عن مؤلف المستند. لتحقيق هذا الغرض ، يقارن المرء نص الاستعلام بنموذج المؤلف المرشح ويحدد احتمال نموذج الاستعلام.
-
-Arabic authorship attribution is the task of finding the author of a document.
-To achieve this purpose, one compares a query text with a model of the candidate author and determines the likelihood of the model for the query.
-	""")
-
-#text = st.text_area("Authorship Attribution Check","Enter Text Here")
-
-st.write('---')
-st.write('## Contact Our Group')
-
-
-st.write("""
-[Authorship Attribution](https://github.com/A-safarji) - feel free to contact!
-""")
-      
-
-df = pd.read_csv('aotures.csv', index_col=0)	
-df
-
-
+from transformers import TFBertModel
+from transformers import BertTokenizer
 from tensorflow.keras.layers import (
     Dense,
     Flatten,
@@ -60,8 +16,6 @@ from tensorflow.keras.layers import (
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import Model
-from transformers import TFBertModel
-from transformers import BertTokenizer
 model_name = "aubmindlab/bert-base-arabertv2"
 max_length = 512
 batch_size = 128
@@ -69,7 +23,6 @@ num_class = 95
 
 tf.compat.v1.logging.set_verbosity(2)
 tokenizer = BertTokenizer.from_pretrained(model_name)
-
 
 try:
     tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
@@ -124,6 +77,9 @@ def load_model():
 
 model= load_model()
 st.write(model)
+
+
+
 # if __name__ == '__main__':
 #     st.title('My app')
 #     sentence = st.text_input('Input your sentence here:')
@@ -153,4 +109,48 @@ st.write(model)
 # ).cache()
 # y_pred = model.predict(p, verbose=2)
 
-                         
+        
+	
+
+	
+	
+#pio.renderers.default = 'chrome'
+st.set_option('deprecation.showPyplotGlobalUse', False)
+#st.set_page_config(layout="wide")
+
+#st.title('Recommended for you!')
+st.markdown(' <p align="center" class="big-font">  <b>Authorship Attribution <u> 🌟 T5 🇸🇦</b>   </p>', unsafe_allow_html=True)	
+
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:50px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+
+
+st.markdown("""
+إسناد التأليف العربي هو مهمة البحث عن مؤلف المستند. لتحقيق هذا الغرض ، يقارن المرء نص الاستعلام بنموذج المؤلف المرشح ويحدد احتمال نموذج الاستعلام.
+
+Arabic authorship attribution is the task of finding the author of a document.
+To achieve this purpose, one compares a query text with a model of the candidate author and determines the likelihood of the model for the query.
+	""")
+
+#text = st.text_area("Authorship Attribution Check","Enter Text Here")
+
+st.write('---')
+st.write('## Contact Our Group')
+
+
+st.write("""
+[Authorship Attribution](https://github.com/A-safarji) - feel free to contact!
+""")
+      
+
+df = pd.read_csv('aotures.csv', index_col=0)	
+df
+
+
