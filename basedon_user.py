8f294f68e0cb7bd85320dfe99f919f454e9dc3c9
A.Safarji <a.m.safarji@gmail.com>
2021-12-08 14:45:47 +0300

    Update basedon_user.py
    NLP

--- basedon_user.py
+++ basedon_user.py
@@ -4,19 +4,19 @@ import pandas as pd
 import tensorflow as tf
 import logging
 import pandas as pd
-from transformers import logging
-from transformers import TFBertModel
-from transformers import BertTokenizer
-from tensorflow.keras.layers import (
-    Dense,
-    Flatten,
-    Conv1D,
-    Dropout,
-    Input,
-)
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import Model
+# from transformers import logging
+# from transformers import TFBertModel
+# from transformers import BertTokenizer
+# from tensorflow.keras.layers import (
+#     Dense,
+#     Flatten,
+#     Conv1D,
+#     Dropout,
+#     Input,
+# )
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.optimizers import Adam
+# from tensorflow.keras import Model
 # model_name = "aubmindlab/bert-base-arabertv2"
 # max_length = 512
 # batch_size = 128
@@ -55,7 +55,7 @@ except ValueError:
 #     model = Model(inputs=input_word_ids, outputs=output)
 #     return model
 
-from keras import backend as K
+#from keras import backend as K
 
 #steps_per_exe =32
 
